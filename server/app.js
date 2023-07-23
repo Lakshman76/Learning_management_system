034e48f53d8d91a8116c66dda21d1b244f2c0a0a
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-23 15:42:46 +0530

    server created successfully

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use('/ping', (req, res)=>{
+    res.send('pong');
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('OOPS!! 404 page not found');
+})
+
+module.exports = app;
