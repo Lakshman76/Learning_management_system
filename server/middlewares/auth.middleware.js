0dfcb3728f73735c58486befc66f76014e36caab
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-26 17:47:19 +0530

    Authentication middleware

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import AppError from "../utils/appError";
+
+const isLoggedIn = function(req, res, next){
+    const {token} = req.cookies;
+
+    if(!token){
+        return next(new AppError('UnAuthenticated, please login!', 401));
+    }
+
+    const tokenDetails = jwt.verify(token, process.env.JWT_SECRET);
+
+    if(!tokenDetails){
+        return next(new AppError('UnAuthenticated, please login!', 401));
+    }
+    req.user = tokenDetails;
+
+    next();
+}
+
+module.exports = {
+    isLoggedIn
+}
