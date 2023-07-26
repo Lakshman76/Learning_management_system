22dca315770db767d437a32614f4f0299f2e85b1
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-26 17:33:11 +0530

    Logout controller done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -74,8 +74,17 @@ const login = async ()=>{
     })
 }
 
-const logout = async ()=>{
+const logout = (req, res)=>{
+    res.cookie("token", null, {
+        secure: true,
+        maxAge: 0,
+        httpOnly: true
+    })
 
+    res.status(200).json({
+        success: true,
+        message: "User logged out successfully"
+    })
 }
 const getProfile = async ()=>{
 
