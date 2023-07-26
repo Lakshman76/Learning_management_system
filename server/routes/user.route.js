1079b3f89d0a67c99e4d3ac9589f9627cde95bba
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-26 17:54:53 +0530

    User routes completed

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,9 +3,11 @@ const { register, login, logout, getProfile } = require('../controllers/user.con
 
 const router = express.Router();
 
+const {isLoggedIn} = require('../middlewares/auth.middleware');
+
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.get('/me', getProfile);
+router.get('/me',isLoggedIn, getProfile);
 
 module.exports = router;
