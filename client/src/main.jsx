7e1cabec0695d204a59428adcaef6477e927708f
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-06-11 13:51:59 +0530

    Add react-hot-toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
-import './index.css'
+import "./index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster />
+  </BrowserRouter>
+);
