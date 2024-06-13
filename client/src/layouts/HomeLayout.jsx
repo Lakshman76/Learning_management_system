f7b26330a3077e0056f9b32c457f0a185fbf7003
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-06-13 15:14:02 +0530

    add login signup button to sidebar

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -1,29 +1,38 @@
 import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom";
 
 import Footer from "../components/Footer";
 
 const HomeLayout = ({ children }) => {
-  
   function changeWidth() {
     const drowerSide = document.getElementsByClassName("drawer-side");
     drowerSide[0].style.width = "auto";
   }
 
-  function hideDrawer(){
+  function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
     const drowerSide = document.getElementsByClassName("drawer-side");
     drowerSide[0].style.width = 0;
   }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+  const role = useSelector((state) => state?.auth?.role);
+
+  function onLogout(e) {
+    e.preventDefault();
+    // TODO:
+    navigate("/");
+  }
   return (
     <div className="min-h-[90vh]">
       <div className="drawer absolute left-0 z-50">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
-          {/* Page content here */}
           <label htmlFor="my-drawer">
             <FiMenu
               onClick={changeWidth}
@@ -43,6 +52,11 @@ const HomeLayout = ({ children }) => {
             <li>
               <Link to={"/"}>Home</Link>
             </li>
+            {isLoggedIn && role === "ADMIN" && (
+              <li>
+                <Link to={"/admin/dashboard"}>Admin Dashboard</Link>
+              </li>
+            )}
             <li>
               <Link to={"/about"}>About Us</Link>
             </li>
@@ -52,6 +66,31 @@ const HomeLayout = ({ children }) => {
             <li>
               <Link to={"/courses"}>All Courses</Link>
             </li>
+            { !isLoggedIn ? (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="btn btn-primary px-4 py-1 font-semibold rounded-md w-1/2">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-1/2">
+                    <Link to="/signup">Signup</Link>
+                  </button>
+                </div>
+
+              </li>
+            ) : (
+              <li className="absolute bottom-4 w-[90%]">
+              <div className="w-full flex items-center justify-center">
+                <button className="btn btn-primary px-4 py-1 font-semibold rounded-md w-1/2">
+                  <Link to="/user/profile">Profile</Link>
+                </button>
+                <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-1/2">
+                  <Link onClick={onLogout}>Signup</Link>
+                </button>
+              </div>
+
+            </li>
+            )}
           </ul>
         </div>
       </div>
