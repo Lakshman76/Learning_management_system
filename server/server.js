5d10475e6b95c378c962d4d6b94a70c41a19c4c3
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-06-18 13:37:48 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 5011;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
     console.log(`App running at http://localhost:${PORT}`);
