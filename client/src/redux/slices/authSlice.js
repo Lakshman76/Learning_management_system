473eda880e92648c72e18d962f0258ba00e97135
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-06-18 14:05:53 +0530

    implement async thunk

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -1,10 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+import axiosInstance from "../../config/axiosInstance";
 
 const initialState = {
   isLoggedIn: localStorage.getItem("isLoggedIn") || false,
   role: localStorage.getItem("role") || "",
   data: localStorage.getItem("data") || {},
 };
+
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+  try {
+    const response = axiosInstance.post("user/register", data);
+    toast.promise(response, {
+      loading: "Wait! creating your account",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed to create your account",
+    });
+    return await response;
+  } catch (error) {
+    console.log(error); 
+    toast.error(error?.response?.data?.message);
+  }
+});
 
 const authSlice = createSlice({
   name: "auth",
