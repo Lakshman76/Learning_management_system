5fdbd30b79c68b023cec8abd724e6f6903f7feb4
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-10 16:23:59 +0530

    upload user profile picture to cloudinary

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,112 +1,135 @@
-import User from '../models/user.model.js';
-import AppError from '../utils/appError.js';
+import User from "../models/user.model.js";
+import AppError from "../utils/appError.js";
+import cloudinary from "cloudinary";
+import fs from "fs/promises";
 
 const cookieoptions = {
-    secure: true,
-    maxAge: 7*24*60*60*1000, // 7 days
-    httpOnly: true
-}
+  secure: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  httpOnly: true,
+};
 
-const register = async (req, res, next)=>{
-    const { fullName, email, password } = req.body;
+const register = async (req, res, next) => {
+  const { fullName, email, password } = req.body;
 
-    console.log("hello",req.body);
+  if (!fullName || !email || !password) {
+    return next(new AppError("All fields are required", 400));
+  }
 
-    if(!fullName || !email || !password){
-        return next(new AppError('All fields are required', 400));
-    }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return next(new AppError("Email already Exists", 400));
+  }
 
-    const userExists = await User.findOne({email});
-    if (userExists) {
-        return next(new AppError('Email already Exists', 400));
-    }
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    avatar: {
+      public_id: email,
+      secure_url:
+        "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=",
+    },
+  });
 
-    const user = await User.create({
-        fullName,
-        email,
-        password,
-        avatar:{
-            public_id: email,
-            secure_url: 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI='
-        }
-    });
+  if (!User) {
+    return next(
+      new AppError("User registration failed, please try again!", 400)
+    );
+  }
 
-    if(!User){
-        return next(new AppError('User registration failed, please try again!', 400))
-    }
-    // TODO: upload user picture
-
-    await user.save();
-
-    // TODO: set JWT token in cookie
-
-    user.password = undefined;
-
-    res.status(200).json({
-        success:true,
-        message:'User registered successfully',
-        user
-    })
-}
-
-const login = async (req, res, next)=>{
-    const {email, password} = req.body;
-
-    if(!email || !password){
-        return next(new AppError('All fields are required', 400));
-    }
-    const user = await User.findOne({
-        email
-    }).select('+password');
-
-    if(!user || !user.comparePassword(password)){
-        return next(new AppError('Email or password doesnot match', 400));
-    }
-
-    const token = await user.generateJWTToken();
-
-    user.password = undefined;
-
-    res.cookie('token', token, cookieoptions);
-
-    res.status(201).json({
-        success: true,
-        message: "User Regisered Successfully",
-        user
-    })
-}
-
-const logout = (req, res)=>{
-    res.cookie("token", null, {
-        secure: true,
-        maxAge: 0,
-        httpOnly: true
-    })
-
-    res.status(200).json({
-        success: true,
-        message: "User logged out successfully"
-    })
-}
-const getProfile = async (req, res)=>{
+  if (req.file) {
     try {
-        const user = await User.findById(req.user.id);
-        res.status(200).json({
-            success: true,
-            message: 'User Details',
-            user
-        })
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            message: err.message
-        })
-    }
-}
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: "LEARNING_MANAGEMENT_SYSTEM",
+        width: 250,
+        height: 250,
+        gravity: "faces",
+        crop: "fill",
+      });
+      if (result) {
+        user.avatar.public_id = result.public_id;
+        user.avatar.secure_url = result.secure_url;
 
-export {
-    register,
-    login,
-    logout,
-    getProfile
-}
+        fs.rm(`uploads/${req.file.filename}`);
+      }
+    } catch (e) {
+      return next(new AppError("File not uploaded, please try again!", 500));
+    }
+  }
+
+  await user.save();
+
+  const token = await user.generateJWTToken();
+
+  res.cookie("token", token, cookieoptions);
+
+  user.password = undefined;
+
+  res.status(200).json({
+    success: true,
+    message: "User registered successfully",
+    user,
+  });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("All fields are required", 400));
+  }
+  const user = await User.findOne({
+    email,
+  }).select("+password");
+
+  if (!user || !user.comparePassword(password)) {
+    return next(new AppError("Email or password doesnot match", 400));
+  }
+
+  const token = await user.generateJWTToken();
+
+  user.password = undefined;
+
+  res.cookie("token", token, cookieoptions);
+
+  res.status(201).json({
+    success: true,
+    message: "User Regisered Successfully",
+    user,
+  });
+};
+
+const logout = (req, res) => {
+  res.cookie("token", null, {
+    secure: true,
+    maxAge: 0,
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "User logged out successfully",
+  });
+};
+const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+      success: true,
+      message: "User Details",
+      user,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+const forgotPassword = async (req, res, next) => {};
+
+const resetPassword = async (req, res, next) => {};
+
+export { register, login, logout, getProfile, forgotPassword, resetPassword };
