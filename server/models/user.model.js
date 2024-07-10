c73ea58ebc2492c1b61fafedfc21fdc80b39b246
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-10 16:25:24 +0530

    add generateForgotToken method

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,69 +1,90 @@
-import {Schema, model} from 'mongoose';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new Schema({
-    fullName:{
-        type:String,
-        required:[true, "Name is required"],
-        minLength:[5, "Name must be atleast 5 character"],
-        maxLength:[30, "Name must be less than 30 character"],
-        trim:true
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Name is required"],
+      minLength: [5, "Name must be atleast 5 character"],
+      maxLength: [30, "Name must be less than 30 character"],
+      trim: true,
     },
-    email:{
-        type:String,
-        required:[true, "Email is required"],
-        lowercase:true,
-        unique:true,
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Invalid email address'
-          ]
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Invalid email address",
+      ],
     },
-    password:{
-        type:String,
-        required:[true, "Password is required"],
-        select:false
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
     },
-    role:{
-        type:String,
-        enum:["USER", "ADMIN"],
-        default:"USER"
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
     },
-    avatar:{
-        public_id:{
-            type:String
-        },
-        secure_url:{
-            type:String
-        }
+    avatar: {
+      public_id: {
+        type: String,
+      },
+      secure_url: {
+        type: String,
+      },
     },
     forgotPasswordToken: String,
-    forgotPasswordExpiryDate: Date 
-}, {
-    timestamps:true
-})
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.pre('save', async function(){
-    if(!this.isModified('password')){
-        return next();
-    }
-    this.password = await bcrypt.hash(this.password, 8);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 8);
 });
 
 userSchema.methods = {
-    comparePassword: async function(plainTextPassword){
-        return await bcrypt.compare(plainTextPassword, this.password);
-    },
-    generateJWTToken:function(){
-        return jwt.sign(
-            {id: this._id, role: this.role, email:this.email, subscription: this.subscription},
-            process.env.SECRET,
-            {expiresIn: process.env.JWT_EXPIRY}
-        )
-    }
-}
+  comparePassword: async function (plainTextPassword) {
+    return await bcrypt.compare(plainTextPassword, this.password);
+  },
+  generateJWTToken: function () {
+    return jwt.sign(
+      {
+        id: this._id,
+        role: this.role,
+        email: this.email,
+        subscription: this.subscription,
+      },
+      process.env.SECRET,
+      { expiresIn: process.env.JWT_EXPIRY }
+    );
+  },
+  generateForgotToken: async function () {
+    const resetToken = crypto.randomBytes(20).toString("hex");
 
-const User = model('User', userSchema);
+    this.forgotPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+
+    this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000;
+
+    return resetToken;
+  },
+};
+
+const User = model("User", userSchema);
 
 export default User;
