faa603c222fb5f72e3b33397bd45438ad300cd50
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-11 22:37:42 +0530

    move dotenv from server.js to app.js

--- server/app.js
+++ server/app.js
@@ -1,35 +1,39 @@
-import express from'express';
-import cookieParser from'cookie-parser';
-import cors from'cors';
-import dbConnect from'./config/dbConnection.js';
-import userRoutes from'./routes/user.route.js';
-import errorMiddleware from'./middlewares/error.middleware.js';
-import morgan from 'morgan';
+import { config } from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dbConnect from "./config/dbConnection.js";
+import userRoutes from "./routes/user.route.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import morgan from "morgan";
 
+config();
 dbConnect();
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({
+app.use(
+  cors({
     origin: [process.env.FRONTEND_URL],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/ping', (req, res)=>{
-    res.send('pong');
-})
+app.use("/ping", (req, res) => {
+  res.send("pong");
+});
 
-app.use('/api/v1/user', userRoutes);
+app.use("/api/v1/user", userRoutes);
 
-app.all('*', (req, res)=>{
-    res.status(404).send('OOPS!! 404 page not found');
-})
+app.all("*", (req, res) => {
+  res.status(404).send("OOPS!! 404 page not found");
+});
 
 app.use(errorMiddleware);
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
-import {config}  from 'dotenv';
-import app from './app.js';
-import cloudinary from 'cloudinary';
-
-config();
+import app from "./app.js";
+import cloudinary from "cloudinary";
 
 const PORT = process.env.PORT || 5000;
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-app.listen(PORT, ()=>{
-    console.log(`App running at http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App running at http://localhost:${PORT}`);
+});
