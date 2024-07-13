d6c87cbd2936e39ae716eb620110ce26b52d971c
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-13 15:56:14 +0530

    add try catch to overcome error

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,24 +1,25 @@
 import AppError from "../utils/appError.js";
-
 import jwt from "jsonwebtoken";
 
-const isLoggedIn = function(req, res, next){
-    const {token} = req.cookies;
+const isLoggedIn = function (req, res, next) {
+  try {
+    const { token } = req.cookies;
 
-    if(!token){
-        return next(new AppError('UnAuthenticated, please login!', 401));
+    if (!token) {
+      return next(new AppError("UnAuthenticated, please login!", 401));
     }
 
     const tokenDetails = jwt.verify(token, process.env.JWT_SECRET);
 
-    if(!tokenDetails){
-        return next(new AppError('UnAuthenticated, please login!', 401));
+    if (!tokenDetails) {
+      return next(new AppError("UnAuthenticated, please login!", 401));
     }
     req.user = tokenDetails;
 
     next();
-}
+  } catch (err) {
+    return next(new AppError("UnAuthenticated, please login!", 401));
+  }
+};
 
-export {
-    isLoggedIn
-}
+export { isLoggedIn };
