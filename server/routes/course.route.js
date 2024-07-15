c9039aa86b8287ab3de4e54f37236af501932596
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-15 22:06:00 +0530

    define route for get lecture by id

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getAllCourses } from "../controllers/course.controller.js";
+import {
+  getAllCourses,
+  getLecturesByCourseId,
+} from "../controllers/course.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.route("/").get(getAllCourses);
+router.route("/:courseId").get(isLoggedIn, getLecturesByCourseId);
 
 export default router;
