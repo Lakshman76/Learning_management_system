11fcb873a38825a57da359e2cb8cb3c6a6b53e93
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-15 22:06:43 +0530

    implement getLecturesByCourseId functionality

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -3,7 +3,7 @@ import AppError from "../utils/appError.js";
 
 export const getAllCourses = async (req, res, next) => {
   try {
-    const courses = Course.find({}).select("-lectures");
+    const courses = await Course.find({}).select("-lectures");
     res.status(200).json({
         success: true,
         message: "All courses",
@@ -13,3 +13,23 @@ export const getAllCourses = async (req, res, next) => {
     return next(new AppError(err.message, 500));
   }
 };
+
+export const getLecturesByCourseId = async (req, res, next) => {
+    try {
+        const { courseId } = req.params;
+        const course = await Course.findById(courseId);
+
+        if(!course){
+            return next(new AppError("Invalid course Id!", 400));
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Course lectures fetched successfully",
+            lectures: course.lectures,
+        })
+    } catch (err) {
+        
+    }
+
+}
