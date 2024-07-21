3f0123ed950fe3de55e9c0e80aa1636a5179248e
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-21 17:02:52 +0530

    make supportable for .gif

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -7,17 +7,25 @@ const upload = multer({
   storage: multer.diskStorage({
     destination: "uploads/",
     filename: (_req, file, cb) => {
-        cb(null, file.originalname);
-    }
+      cb(null, file.originalname);
+    },
   }),
   fileFilter: (_req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if(ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".webp" && ext !== ".jpg" &&ext !== ".mp4"){
-        cb(new Error(`unsuported file type! ${ext}`), false);
-        return;
+    if (
+      ext !== ".jpg" &&
+      ext !== ".jpeg" &&
+      ext !== ".png" &&
+      ext !== ".webp" &&
+      ext !== ".jpg" &&
+      ext !== ".mp4" &&
+      ext !== ".gif"
+    ) {
+      cb(new Error(`unsuported file type! ${ext}`), false);
+      return;
     }
-    cb(null, true)
-  }
+    cb(null, true);
+  },
 });
 
 export default upload;
