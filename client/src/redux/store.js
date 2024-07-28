61e1db62df4b94e8666f952a7aa00e2abc6744fc
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-07-28 14:52:31 +0530

    lecture reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/authSlice";
 import courseReducer from "./slices/courseSlice";
+import lectureReducer from "./slices/lectureSlice"
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    course: courseReducer
+    course: courseReducer,
+    lecture: lectureReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
   devTools: true,
