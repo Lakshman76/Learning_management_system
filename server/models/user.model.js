e91512bee6a30db09e7e759120a2b3be8eeccc3f
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-15 11:16:36 +0530

    add new schema "subscription" for useSchema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -42,6 +42,10 @@ const userSchema = new Schema(
     },
     forgotPasswordToken: String,
     forgotPasswordExpiry: Date,
+    subscription: {
+      id: String,
+      status: String
+    }
   },
   {
     timestamps: true,
