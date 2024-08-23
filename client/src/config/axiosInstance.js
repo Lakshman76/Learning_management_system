7a2efdb8eb465101fb57452884dd91f912197d7e
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-23 14:28:08 +0530

    deploy

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1"
+const BASE_URL = "https://learning-management-system-lakshman76-lakshman76s-projects.vercel.app/api/v1"
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
