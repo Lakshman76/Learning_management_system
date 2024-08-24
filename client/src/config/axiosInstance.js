25001e4113c38f6f82d42d5b31a2eb04a22c7377
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-24 12:10:12 +0530

    set env and vercel.json

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://learning-management-system-lakshman76-lakshman76s-projects.vercel.app/api/v1"
+
+const BASE_URL = `${import.meta.env.BACKEND_URL}/api/v1`
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
