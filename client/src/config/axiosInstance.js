6523f62137c1ad7d43e67222fbb2100a1d09782f
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-26 21:11:49 +0530

    update import type for vite

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = `${import.meta.env.BACKEND_URL}/api/v1`
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1/`
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
